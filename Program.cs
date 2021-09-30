3c58b6cf59fe1c4623c6303b7ccfb22c50fc9fd6
wasif farid <wsffarid1@gmail.com>
2021-09-30 13:38:53 +0530

    push method implemented

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to stack and Queue");
+            Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(53);
+            Console.WriteLine("Stack is created and elements are pushed");
         }
     }
 }
---
+++ Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    public class StackNode<T>
+    {
+        public T value;
+        public StackNode<T> Next;
+        public StackNode(T value)
+        {
+            this.value = value;
+            this.Next = null;
+        }
+    }
+    public class Stack<T>
+    {
+        public StackNode<T> top;
+
+        public void Push(T value)
+        {
+            StackNode<T> stackNode = new StackNode<T>(value);
+            if (top == null)
+            {
+                top = stackNode;
+            }
+            else
+            {
+                stackNode.Next = top;
+                top = stackNode;
+            }
+        }
+
+    }
+}
